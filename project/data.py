39d739008bfad8bcaf67e54eeb92c57742f15249
Ming Qian <mqian.coding@gmail.com>
2023-07-18 00:22:50 -0700

    move data up

--- project/data.py
+++ project/data.py
@@ -10,15 +10,15 @@ ERR_NO_PRICE_DATA = "error: no price data"
 def get_current_price(symbol: str):
     # TODO: Error handling + Context Deadline
     t = yf.Ticker(symbol)
-    if t and t.fast_info and "lastPrice" in t.fast_info:
-        return t.fast_info.get("lastPrice")
+    if t and t.fast_info:
+        return t.fast_info.get("lastPrice", ERR_NO_PRICE_DATA)
     return ERR_NO_PRICE_DATA
 
 
 def start_archiving(symbol: str, frequency: int):
     while True:
         today = dt.now().date()
-        with open(f"./test/price_data/{symbol}_{today.year}_{today.month}_{today.day}.csv", 'w', newline='') as file:
+        with open(f"./price_data/{symbol}_{today.year}_{today.month}_{today.day}.csv", 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(["timestamp", "price"])
             while dt.now().date() == today:
